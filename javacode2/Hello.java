9db2da153a2f6d7287731f8a112cc59c0bef0aaa
sourav-009 <sourabh4321k@gmail.com>
2026-07-17 12:24:03 +0530

    JAVA

---
+++ javacode2/Hello.java
@@ -0,0 +1,10 @@
+public class Hello{
+	public static void main(String args[]){
+		System.out.println("Hello");
+		int a = 10, b = 10;
+		int c = a + b;
+		System.out.println("Sum is "+c);
+	}
+}
+// There are 6 keywords
+// 16 Identifier
---
+++ javacode2/ShorthandOperatorEx.java
@@ -0,0 +1,29 @@
+public class ShorthandOperatorEx {
+
+	public static void main(String[] args) {
+		int a=10;
+		a *= 5;
+		System.out.println(a);
+		
+		a=10;
+		a /= 5;
+		System.out.println(a);
+		
+		a=10;
+		a %= 5;
+		System.out.println(a);
+		
+		a=10;
+		a %= 15;
+		System.out.println(a);
+
+		a=10;
+		a =+ 5;
+		System.out.println(a);
+		
+		a=10;
+		a =- 5;
+		System.out.println(a);
+	}
+
+}
