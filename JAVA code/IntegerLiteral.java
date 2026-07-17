3da0991fb822703c0b3f0f5db281c3d5b6d2f1e9
sourav-009 <sourabh4321k@gmail.com>
2026-07-17 12:21:15 +0530

    JAVA

---
+++ JAVA code/IntegerLiteral.java
@@ -0,0 +1,14 @@
+class second
+{
+    public static void main(String[] args)
+    { 
+        int a = 104;
+        int b = 0150;
+        int c = 0b1101000;
+        int d = 0*68
+        System.out.print(a);
+        System.out.print(b);
+        System.out.print(c);
+        System.out.print(d);
+    } 
+}
---
+++ JAVA code/public class DMQues6
@@ -0,0 +1,13 @@
+public class DMQues6 {
+
+	public static void main(String[] args) {
+		int a=10,b=5;
+		if(a++>b--) { 
+			System.out.println(a+b);
+		}else {
+			a++;
+		}
+		System.out.println(""+a+b);
+	}
+
+}
