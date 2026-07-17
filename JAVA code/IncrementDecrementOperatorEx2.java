4bccbb477c18b1a9bd445cb1d3bb54e3b767a46f
sourav-009 <sourabh4321k@gmail.com>
2026-07-17 12:31:14 +0530

    JAVA

---
+++ JAVA code/IncrementDecrementOperatorEx2.java
@@ -0,0 +1,18 @@
+public class IncrementDecrementOperatorEx2 {
+
+	public static void main(String[] args) {
+		int a=10;
+		a++;
+		System.out.println(a);
+		int b=10;
+		++b;
+		System.out.println(b);
+		int c=10;
+		c--;
+		System.out.println(c);
+		int d=10;
+		--d;
+		System.out.println(d);
+	}
+
+}
---
+++ JAVA code/IncrementDecrementOperatorEx3.java
@@ -0,0 +1,10 @@
+public class IncrementDecrementOperatorEx3 {
+
+	public static void main(String[] args) {
+		int a=10,b;
+		b=a++;
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
